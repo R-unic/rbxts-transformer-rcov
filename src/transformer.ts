5fe31c7ea122c0c9694ae93a0c104d20686c538d
R-unic <roleyspersonal@gmail.com>
2025-03-16 19:25:46 -0400

    feat: manual function-like declaration transformation

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,4 +1,4 @@
-import ts from "typescript";
+import ts, { ConciseBody } from "typescript";
 
 /**
  * This is the transformer's configuration, the values are passed from the tsconfig.
@@ -70,11 +70,48 @@ function visitExpression(context: TransformContext, node: ts.Expression): ts.Exp
   return context.transform(node);
 }
 
+function visitBlock(context: TransformContext, node: ts.Block): ts.Block {
+  return context.factory.updateBlock(node, node.statements.map(s => visitStatement(context, s)).flat());
+}
+
+function visitFunctionLikeDeclaration(context: TransformContext, node: ts.FunctionLikeDeclaration): ts.FunctionLikeDeclaration {
+  const { factory } = context;
+  let newBody = node.body as ts.Block | undefined;
+  if (node.body !== undefined)
+    if (ts.isBlock(node.body))
+      newBody = visitBlock(context, node.body);
+    else if (ts.isExpression(node.body))
+      newBody = visitBlock(context, factory.createBlock([factory.createExpressionStatement(node.body)]));
+
+  if (node.body === newBody)
+    return context.transform(node);
+
+  if (ts.isFunctionDeclaration(node))
+    return factory.updateFunctionDeclaration(node, node.modifiers, node.asteriskToken, node.name, node.typeParameters, node.parameters, node.type, newBody);
+  else if (ts.isFunctionExpression(node))
+    return factory.updateFunctionExpression(node, node.modifiers, node.asteriskToken, node.name, node.typeParameters, node.parameters, node.type, newBody!);
+  else if (ts.isArrowFunction(node))
+    return factory.updateArrowFunction(node, node.modifiers, node.typeParameters, node.parameters, node.type, node.equalsGreaterThanToken, newBody as ConciseBody);
+  else if (ts.isMethodDeclaration(node))
+    return factory.updateMethodDeclaration(node, node.modifiers, node.asteriskToken, node.name, node.questionToken, node.typeParameters, node.parameters, node.type, newBody);
+  else if (ts.isGetAccessorDeclaration(node))
+    return factory.updateGetAccessorDeclaration(node, node.modifiers, node.name, node.parameters, node.type, newBody);
+  else if (ts.isSetAccessorDeclaration(node))
+    return factory.updateSetAccessorDeclaration(node, node.modifiers, node.name, node.parameters, newBody);
+
+  return factory.updateConstructorDeclaration(node, node.modifiers, node.parameters, newBody);
+}
+
 function visitNode(context: TransformContext, node: ts.Node): ts.Node | ts.Node[] {
+  console.log(ts.SyntaxKind[node.kind], ts.isStatement(node) || ts.isBlock(node) || ts.isFunctionLikeDeclaration(node));
   if (ts.isStatement(node))
     return visitStatement(context, node);
   else if (ts.isExpression(node))
-    return visitExpression(context, node);
+    return visitExpression(context, node)
+  else if (ts.isBlock(node))
+    return visitBlock(context, node);
+  else if (ts.isFunctionLikeDeclaration(node))
+    return visitFunctionLikeDeclaration(context, node);
 
   // We encountered a node that we don't handle above,
   // but we should keep iterating the AST in case we find something we want to transform.
